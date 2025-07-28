5c3248ccd8db2dedf7efce8399421fe36244be7a
sureyya isik <156483652+sureyyaisik@users.noreply.github.com>
2025-07-28 14:27:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let hamburger = document.querySelector(".hamburger");
+    hamburger.onclick = function () {
+        let navBar = document.querySelector(".nav-bar");
+        navBar.classList.toggle("active");
+    };
+});
